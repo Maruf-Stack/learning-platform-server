b781898b8fd117666bd1fb4e33e46f77d7d20bec
Maruf <01780527702maruf@gmail.com>
2022-10-25 17:00:10 +0600

    catagory added

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const port = process.env.Port || 5000;
-
+app.use(cors())
 const courses = require('./data/courses.json');
 app.get('/', (req, res) => {
     res.send('server is running')
