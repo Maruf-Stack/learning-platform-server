5ef959a7f274580e2e096abe8cd611569b0d7695
Maruf <01780527702maruf@gmail.com>
2022-10-25 13:44:14 +0600

    all courses send to server

--- index.js
+++ index.js
@@ -3,10 +3,14 @@ const app = express();
 const cors = require('cors');
 const port = process.env.Port || 5000;
 
+const course = require('./data/courses.json');
 app.get('/', (req, res) => {
     res.send('server is running')
 })
-app.get('/courseds', (req, res))
+app.get('/courses', (req, res) => {
+    res.send(course)
+})
+
 app.listen(port, () => {
     console.log('server is running on ', port)
 })
