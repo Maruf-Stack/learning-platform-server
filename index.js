5519b2984931f5a9d1d8a2cd5479379fb1059481
Maruf <01780527702maruf@gmail.com>
2022-10-25 13:49:19 +0600

    single course id added

--- index.js
+++ index.js
@@ -3,14 +3,18 @@ const app = express();
 const cors = require('cors');
 const port = process.env.Port || 5000;
 
-const course = require('./data/courses.json');
+const courses = require('./data/courses.json');
 app.get('/', (req, res) => {
     res.send('server is running')
 })
 app.get('/courses', (req, res) => {
+    res.send(courses)
+})
+app.get('/courses/:id', (req, res) => {
+    const id = req.params.id;
+    const course = courses.find(c => c.id == id);
     res.send(course)
 })
-
 app.listen(port, () => {
     console.log('server is running on ', port)
 })
